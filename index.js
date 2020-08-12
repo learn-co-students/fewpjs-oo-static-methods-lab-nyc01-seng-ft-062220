c0b8e87c718422e43b38da64ecdf9f3093639544
Jeff-Adler <Jeffm.adler@gmail.com>
2020-08-12 14:54:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 class Formatter {
   //add static methods here
   static capitalize(string) {
-    return string.charAt(0).toUpperCase()
+    return string.charAt(0).toUpperCase() + string.slice( 1 )
   }
 
   static sanitize(string) {
     return string.replace(/[^A-Za-z0-9 '-]/g, '');
   }
-  
+
   static titleize( sentence ) {
     let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
     let result = [];
